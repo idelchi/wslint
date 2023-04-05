afe025b23ef0625c928495b2b9446ade9268269f
Arash Idelchi <64003206+idelchi@users.noreply.github.com>
2023-04-05 21:07:06 +0200

    Final touches

--- todos.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// TODO(Idelchi): Add more checkers! (line too long, etc.)
-// TODO(Idelchi): https://medium.com/@hochiho/writing-testable-and-flexible-code-in-golang-2b1a0e66627a
-// TODO(Idelchi): Read https://twin.sh/articles/39/go-concurrency-goroutines-worker-pools-and-throttling-made-simple
-// TODO(Idelchi): Read https://medium.com/@andrewdavisescalona/testing-in-go-some-tools-you-can-use-f3e79b398d8d
