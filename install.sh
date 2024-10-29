435cd7bbc1b17d0d1349c009a092aff59bcb220f
idelchi <64003206+idelchi@users.noreply.github.com>
2024-10-29 10:35:09 +0100

    dev

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+DISABLE_SSL=""
+for arg in "$@"; do
+  if [ "${arg}" = "-k" ]; then
+    DISABLE_SSL=yes
+    break
+  fi
+done
+
+curl ${DISABLE_SSL:+-k} \
+  -sSL \
+  https://raw.githubusercontent.com/idelchi/scripts/refs/heads/dev/install.sh |
+  INSTALLER_TOOL="go-match" \
+    sh -s -- "$@"
